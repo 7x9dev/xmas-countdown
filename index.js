0cab4dc7c7e74479f2ea630c09812327a2055334
7x9dev <103652226+7x9dev@users.noreply.github.com>
2022-10-26 23:28:38 +0300

    Added play button and a song

---
+++ index.js
@@ -0,0 +1,14 @@
+const audio = document.querySelector("#song");
+audio.volume = 0.4;
+
+const btn = document.querySelector("#playBtn");
+let state = "play";
+btn.addEventListener("click", function () {
+   if (state === "play") {
+      document.querySelector("#song").play();
+      state = "pause";
+   } else {
+      document.querySelector("#song").pause();
+      state = "play";
+   }
+});
