ed0e235b831db7f14130a5c49a4483c524fb7cb1
7x9dev <103652226+7x9dev@users.noreply.github.com>
2022-10-26 23:40:43 +0300

    Added countdown

--- index.js
+++ index.js
@@ -12,3 +12,36 @@ btn.addEventListener("click", function () {
       state = "play";
    }
 });
+
+//countdown
+function xmasCountdown() {
+   const xmasDay = new Date("December 25, 2022 00:00");
+   const now = new Date();
+   const timeLeft = xmasDay - now;
+
+   const msInSec = 1000;
+   const msInMin = 60 * 1000;
+   const msInHr = 60 * 60 * 1000;
+   const msInDay = 24 * 60 * 60 * 1000;
+   
+   const displayDay = Math.floor(timeLeft / msInDay);
+   document.querySelector(".days").textContent = displayDay;
+
+   const displayHr = Math.floor(timeLeft%msInDay / msInHr);
+   document.querySelector(".hrs").textContent = displayHr;
+
+   const displayMin = Math.floor((timeLeft % msInHr) / msInMin);
+   document.querySelector(".min").textContent = displayMin;
+
+   const displaySec = Math.floor((timeLeft % msInMin) / msInSec);
+   document.querySelector(".sec").textContent = displaySec;
+
+   if (timeLeft <= 0) {
+      document.querySelector(".days").textContent = 0;
+      document.querySelector(".hrs").textContent = 0;
+      document.querySelector(".min").textContent = 0;
+      document.querySelector(".sec").textContent = 0;
+      clearInterval(timerID);
+   }
+}
+let timerID = setInterval(xmasCountdown, 1000);
